0aa90703b82248183d84b394a33cc891fd8a8969
EduardLev <eduardlev@gmail.com>
2020-05-03 10:56:27 -0400

    Updated Chapter 8

--- CategoryTheory.playground/Pages/Chapter8.xcplaygroundpage/Contents.swift
+++ CategoryTheory.playground/Pages/Chapter8.xcplaygroundpage/Contents.swift
@@ -2,6 +2,20 @@
 
 import Foundation
 
-var str = "Hello, playground"
+//: 5. Define a Bifunctor. Implement bimap
+
+struct Pair<A,B> {
+  let first: A
+  let second: B
+}
+
+func bimap<A,B,C,D>(_ f: @escaping ((A) -> C), _ g: @escaping ((B) -> D)) -> (Pair<A,B>) -> Pair<C,D> {
+  return { abPair in
+    Pair(first: f(abPair.first), second: g(abPair.second))
+  }
+}
+
+let pair = Pair(first: 1, second: "Second")
+print(bimap( { $0 }, { $0 })(pair))
 
 //: [Next](@next)
