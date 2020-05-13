8a53937e5368f0cf235ba4e9373c10f464efb6b4
EduardLev <eduardlev@gmail.com>
2020-05-13 10:01:50 -0400

    Update Chapter 10

--- CategoryTheory.playground/Pages/Chapter10.xcplaygroundpage/Contents.swift
+++ CategoryTheory.playground/Pages/Chapter10.xcplaygroundpage/Contents.swift
@@ -32,6 +32,7 @@ func fmap<A,B>(_ f: @escaping (A) -> B) -> (List<A>) -> List<B> {
   }
 }
 
+// Natural Transformation
 func alpha<A>(optional: Maybe<A>) -> List<A> {
   switch optional {
     case .nothing: return .empty
@@ -48,6 +49,52 @@ print(Gf(alpha(optional: .just(10)))) // list(20, empty)
 // Example: alpha . Ff:
 let Ff: (Maybe) -> Maybe = fmap { $0 + 10 }
 print(alpha(optional: Ff(.just(10)))) // list(20, empty)
+
+//: 2 Define at least two different natural tarnsformations between Reader () and the list functor. How many different lists of () are there?
+
+/*
+ The natural transformations have to have the type of Reader () T -> List<T>.
+ */
+
+struct Reader<T> {
+  let value: () -> T
+}
+
+func natOne<T>(_ reader: Reader<T>) -> List<T> {
+  return .empty
+}
+
+func natTwo<T>(_ reader: Reader<T>) -> List<T> {
+  return .list(reader.value(), .empty)
+}
+
+/*
+ There are infinite different lists of (), because we can insert an arbitrary amount of () values into this returned list.
+ */
+
+//: 3 Continue the previous exercise with Reader Bool and Maybe.
+
+/*
+ The natural transformatons have to have the type of Reader Bool T -> Maybe<T>
+ */
+
+func natThree<T>(_ reader: (Bool) -> T) -> Maybe<T> {
+  return .nothing
+}
+
+func natFour<T>(_ reader: (Bool) -> T) -> Maybe<T> {
+  return .just(reader(true))
+}
+
+func natFive<T>(_ reader: (Bool) -> T) -> Maybe<T> {
+  return .just(reader(false))
+}
+
+/*
+ There are three possible natural transformations from Reader Bool T -> Maybe<T>
+ */
+
+//: 4 Show that the horizontal composition of natural transformation satisfies the naturality condition (hint: use components). It's a good exercise in diagram chasing.
 
 
 
